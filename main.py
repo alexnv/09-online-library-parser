35959e77395a9b67a4fcd64d36b060e83f60ee9b
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-02-06 19:07:36 +0300

    download books from tululu.org.

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from pathlib import Path
+
+import requests
+
+
+def download_book(request_url, base_dir, filename):
+    response = requests.get(request_url)
+    response.raise_for_status()
+    if not base_dir.exists():
+        Path.mkdir(base_dir)
+    file = Path.cwd() / base_dir / filename
+    with open(file, 'wb') as file:
+        file.write(response.content)
+
+
 if __name__ == '__main__':
-    pass
+    url = "https://tululu.org/txt.php?id=32168"
+    base_dir = Path.cwd() / "books"
+    for id in range(10):
+        download_book(url, base_dir, f"id{id}.txt")
