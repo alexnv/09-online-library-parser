161fe693f0e4d65f1047faf2287f19a2dff1d67d
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-06-09 10:24:33 +0300

    Реализован парсинг нескольких страниц

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -10,21 +10,25 @@ def get_soup(genre_page_url):
     return BeautifulSoup(response.text, "lxml")
 
 
-def get_book_rel_path(soup):
-    table = soup.find("table", class_="d_book")
-    path = table.find("a")["href"]
-    return path
+def get_books_rel_paths(soup):
+    tables = soup.find_all("table", class_="d_book")
+    paths = [t.find("a")["href"] for t in tables]
+    return paths
 
 
 def main():
     url = "https://tululu.org/"
     genre = 55
+    total_pages = 10
+    books_rel_paths = []
     page = 1
-    genre_page_url = f"{url}l{genre}/{page}/"
-    soup = get_soup(genre_page_url)
-    book_rel_path = get_book_rel_path(soup)
-    book_url = urljoin(url, book_rel_path)
-    print(book_url)
+    while page <= total_pages:
+        genre_page_url = f"{url}l{genre}/{page}/"
+        soup = get_soup(genre_page_url)
+        books_rel_paths.extend(get_books_rel_paths(soup))
+        page += 1
+    books_urls = [urljoin(url, path) for path in books_rel_paths]
+    print(books_urls)
 
 
 if __name__ == "__main__":
