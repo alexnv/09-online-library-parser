3dde9f675427a15e233a4a7b9874b25c52872dc3
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-06-19 16:41:15 +0300

    Add paginator.

--- render_website.py
+++ render_website.py
@@ -3,29 +3,30 @@ import json
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server, shell
+from livereload import Server
 from more_itertools import chunked
 from pathvalidate import sanitize_filename
 
 
-def livereload_server(rootdir):
-    server = Server()
-    server.watch('./*.jinja', shell('render_website.py', cwd='.'))
-    server.serve(root=".")
-
-
-def render_page(id, books, template, dir):
-    rendered_page = template.render(books=books)
+def render_page(id, totalpages, books, template, dir):
+    pages_nums = range(1, totalpages+1)
+    rendered_page = template.render(
+        books=books,
+        current_page=id+1,
+        total_pages=totalpages,
+        pages_nums=pages_nums
+    )
     filepath = Path.cwd() / dir / sanitize_filename(f"index{id + 1}.html")
     with open(filepath, "w", encoding="utf8") as file:
         file.write(rendered_page)
 
 
-def main():
+def on_reload():
     arguments = create_parser().parse_args()
     book_folder = arguments.book_folder
     json_path = arguments.json_path
     output_path = arguments.output_path
+    books_per_page = arguments.books_per_page
 
     Path(output_path).mkdir(parents=True, exist_ok=True)
 
@@ -40,12 +41,22 @@ def main():
         books_json = books_file.read()
 
     books = json.loads(books_json)
-    books_pages = [chunk for chunk in chunked(books, 10)]
+    books_pages = [chunk for chunk in chunked(books, books_per_page)]
+    totalpages = len(books_pages)
     for index, book_page in enumerate(books_pages):
         books = [chunk for chunk in chunked(book_page, 2)]
-        render_page(index, books, template, output_path)
+        render_page(index, totalpages, books, template, output_path)
 
-    livereload_server(output_path)
+
+def main():
+    arguments = create_parser().parse_args()
+    book_folder = arguments.book_folder
+
+    on_reload()
+    server = Server()
+    server.watch('./*.jinja2', on_reload)
+    server.watch(f"./{book_folder}/*.*", on_reload)
+    server.serve(root=".")
 
 
 def create_parser():
@@ -71,8 +82,14 @@ def create_parser():
     parser.add_argument(
         "-o",
         "--output_path",
-        default="./dist",
+        default="./pages",
         help="Введите путь к каталогу со сгенерированными страницами сайта"
+    )
+    parser.add_argument(
+        "-p",
+        "--books_per_page",
+        default=10,
+        help="Количество книг на странице"
     )
 
     return parser
